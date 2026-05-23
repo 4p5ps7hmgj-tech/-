4b1924e83a9b82310edbc98d87122672727fac72
4p5ps7hmgj-tech <4p5ps7hmgj@privaterelay.appleid.com>
2026-05-24 01:36:40 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -89,7 +89,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#6493a9", 
-      secondary: "#2F3136", 
+      secondary: "#6493a9", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
